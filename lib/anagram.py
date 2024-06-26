811f443d1669259fc0710984c78e4f4fd546582b
Shelli Martinez <shelli.mrtnz@gmail.com>
2024-06-26 17:11:17 -0400

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, list):
+        match_list = []
+        for str in list:
+            if sorted(str) == sorted(self.word):
+                match_list.append(str)
+        return match_list
